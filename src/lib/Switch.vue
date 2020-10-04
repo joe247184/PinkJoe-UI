c6363577276af2f5e43d9409608a27ec70e773e3
Raven <836173483@qq.com>
2020-10-04 15:13:12 +0800

    初步完成switch的css以及js逻辑

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,13 +1,58 @@
 <template>
-  siw
+  <button :class="{checked}" @click="toggleSwitch">
+    <span></span>
+  </button>
 </template>
 
-<script>
+<script lang="ts">
+  import {ref} from "vue";
   export default {
-    name: "Switch"
+    name: "Switch",
+    setup(){
+      const checked = ref(false)
+      const toggleSwitch = () => {
+        checked.value = !checked.value
+      }
+
+      return {
+        toggleSwitch, checked
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  $h: 22px;
+  $h2: $h - 4px;
+  $active-color: #13ce66;
+  $un-active-color: #DCDFE6;
+  button {
+    height: $h;
+    width: $h * 2;
+    border-radius: $h/2;
+    border: none;
+    background-color: $un-active-color;
+    position: relative;
+    outline: none;
+  }
+
+  span {
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: 50%;
+    display: inline-block;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    transition: all .3s;
+  }
+
+  button.checked {
+    background-color: $active-color;
+    > span{
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
 
 </style>
